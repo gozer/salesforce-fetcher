f207a5439e9f68e728c394d570d0925e13266350
Chris Valaas <cvalaas@mozilla.com>
2019-09-06 13:45:38 -0700

    forgot the new library

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(
     install_requires=[
         'Click',
         'simple-salesforce',
+        'salesforce-bulk',
         'pyOpenSSL>=0.14',
         'pyyaml>=3.12',
         'pyjson==1.3.0',
